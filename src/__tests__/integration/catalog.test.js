694604c7652846c1ffc3b31eeb8b2c3ebfe4a5f7
Juscilan Moreto <juscilan.moreto@itlean.com.brL>
2021-03-01 18:54:45 -0300

    feat: adding tests

--- src/__tests__/integration/catalog.test.js
+++ src/__tests__/integration/catalog.test.js
@@ -36,14 +36,48 @@ describe('Catalog API ', () => {
       .get('/catalog')
 
       expect(response.body.lengh > 0)
-      _id = response.body._id
+      _id = response.body[0]._id
   })
 
-  test('Should return a catalog instance - GET /catalog/:id', async ()=> {
+  test('Should return 200OK - GET /catalog/:id', async ()=> {
     const response = await request(app)
-      .get(`/catalog/:${_id}`)
-      console.log(_id)
-      //console.log(response.body)
-    // expect(response.body._id).toBe(_id)
+      .get(`/catalog/${_id}`)
+    
+    expect(response.status).toEqual(200)
+  })
+
+  test('Should return a catalog name - GET /catalog/:id', async ()=> {
+    await request(app)
+      .put(`/catalog`)
+      .send({
+        _id: _id,
+        name: 'MAIONESE HELLMANS',
+        price: 8.90
+      })
+
+    const response = await request(app)
+      .get(`/catalog/${_id}`)
+    
+    expect(response.body.name).toEqual('MAIONESE HELLMANS')
+  })
+
+  test('Should update a catalog name - PUT /catalog', async () => {
+    const response = await request(app)
+      .put(`/catalog`)
+      .send({
+        _id: _id,
+        name: 'MAIONESE HELLMANS LIGH',
+        price: 8.90
+      })
+    expect(response.body.name).toEqual('MAIONESE HELLMANS LIGH')
+  })
+
+  test('Should delete a catalog - DELETE /catalog', async () => {
+    const response = await request(app)
+      .del(`/catalog/${_id}`)
+      .send({
+        _id: _id
+      })
+    expect(response.status).toEqual(200)
   })
 })
--- src/controllers/CatalogController.js
+++ src/controllers/CatalogController.js
@@ -1,8 +1,8 @@
-const Catalog = require('../models/CatalogModel')
+const CatalogModel = require('../models/CatalogModel')
 
 module.exports.create = async (req, res) => {
   try {
-    await Catalog.create(req.body)
+    await CatalogModel.create(req.body)
     res.status(201).end()
   }
   catch(error) {
@@ -12,7 +12,7 @@ module.exports.create = async (req, res) => {
 
 module.exports.getAll = async (_, res) => {
   try {
-    res.status(200).json(await Catalog.find())
+    res.status(200).json(await CatalogModel.find())
   }
   catch(error) {
     res.status(500).json({ errorMessage: error })
@@ -22,21 +22,10 @@ module.exports.getAll = async (_, res) => {
 module.exports.getById = async (req, res) => {
   const catalogId = req.params.id
   try {
-    res.status(200).json(await Catalog.findOne({ _id: catalogId }))
+    res.status(200).json(await CatalogModel.findOne({ _id: catalogId }))
   }
   catch(error) {
     console.log(error)
-    res.status(500).json({ errorMessage: error })
-  }
-}
-
-module.exports.delete = async (req, res) => {
-  const catalogId = req.params.id
-  try {
-    await Catalog.deleteOne({ _id: catalogId })
-    res.status(200).end()
-  }
-  catch(error) {
     res.status(500).json({ errorMessage: error })
   }
 }
@@ -45,25 +34,25 @@ module.exports.update = async (req, res) => {
   const catalogBody = req.body
   const catalogId = req.body._id
   try {
-    const doc = (await Catalog.findOne({ _id: catalogId }))
+    const doc = (await CatalogModel.findOne({ _id: catalogId }))
     if(!doc) {
-      res.status(400).json({ errorMessage: 'Catalog ID not found!' })
+      return res.status(400).json({ errorMessage: 'Catalog ID not found!' })
     }
     await doc.updateOne(catalogBody)
-    res.status(200).end()
+    return res.status(200).json({name: catalogBody.name})
   }
   catch(error) {
-    res.status(500).json({ errorMessage: error })
+    return res.status(500).json({ errorMessage: error })
   }
 }
 
 module.exports.delete = async (req, res) => {
   const catalogId = req.params.id
   try {
-    res.status(200).json(await Catalog.deleteOne({ _id: catalogId }))
+    await CatalogModel.deleteOne({ _id: catalogId })
+    res.status(200).end()
   }
   catch(error) {
-    console.log(error)
     res.status(500).json({ errorMessage: error })
   }
 }
