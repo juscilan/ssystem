e125f5d9a072dff1b18bbc09e373f0352617272a
Juscilan Moreto <juscilan.moreto@itlean.com.brL>
2021-03-01 23:46:54 -0300

    fix: fixing CatalogUtils

--- src/__tests__/integration/unit/CatalogUtils.test.js
+++ src/__tests__/integration/unit/CatalogUtils.test.js
@@ -1,11 +1,11 @@
-const catalogMiddleware = require('../../../utils/Catalog')
+const CatalogUtils = require('../../../utils/Catalog')
 
 describe('Catalog Middleware', () => {
   test('Should return false', () => {
-    expect(catalogMiddleware.isAutenticatedMethod('GET')).toBe(false)
+    expect(CatalogUtils.isAutenticatedMethod('GET')).toBe(false)
   })
 
   test('Should return true', () => {
-    expect(catalogMiddleware.isAutenticatedMethod('POST')).toBe(true)
+    expect(CatalogUtils.isAutenticatedMethod('POST')).toBe(true)
   })
 })
